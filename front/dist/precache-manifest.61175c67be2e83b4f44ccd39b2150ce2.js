5717c6e16909e0b39047d629331d88e85b994827
Eduardo Torres Ferreira <etf1974@hotmail.com>
2020-01-20 15:49:49 -0400

    Added Env, and Heroku

---
+++ front/dist/precache-manifest.61175c67be2e83b4f44ccd39b2150ce2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae5497db69c67dda853",
+    "url": "/css/chunk-53362234.607e9968.css"
+  },
+  {
+    "revision": "c5216d48104dd601dfc3",
+    "url": "/css/chunk-c423d054.d7dd8b43.css"
+  },
+  {
+    "revision": "e70ab0c4ab4d0cda0b0e",
+    "url": "/css/chunk-vendors.ddd23559.css"
+  },
+  {
+    "revision": "726eab432c3db62036a8f814c7169479",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6c10affbef5a318a77d",
+    "url": "/js/app.731392c3.js"
+  },
+  {
+    "revision": "ec3d0c2a82996ef04d5b",
+    "url": "/js/chunk-2d228ec6.22e9406a.js"
+  },
+  {
+    "revision": "3ae5497db69c67dda853",
+    "url": "/js/chunk-53362234.a01b974a.js"
+  },
+  {
+    "revision": "c5216d48104dd601dfc3",
+    "url": "/js/chunk-c423d054.f1cec302.js"
+  },
+  {
+    "revision": "e70ab0c4ab4d0cda0b0e",
+    "url": "/js/chunk-vendors.4036d18f.js"
+  },
+  {
+    "revision": "322c0bf507b3d0ab5eb9ac252ef1f001",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
